dec797d89b243eb1a9937ab431fdaa6eb3b9b4a3
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-07-28 12:28:45 +0300

    Init commit

---
+++ src/main/kotlin/org/pva/KotlinCodewars/KotlinCodewarsApplication.kt
@@ -0,0 +1,11 @@
+package org.pva.KotlinCodewars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCodewarsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCodewarsApplication>(*args)
+}
---
+++ src/main/kotlin/org/pva/KotlinCodewars/kyu8/TotalAmountOfPoints.kt
@@ -0,0 +1,5 @@
+package org.pva.KotlinCodewars.kyu8
+
+fun points(games: List<String>): Int {
+    return 1
+}
