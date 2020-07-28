527aa40d75b2589eabaadaf880bafe3a5fc27417
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-07-28 13:09:52 +0300

    TotalAmountOfPointsTest

--- src/main/kotlin/org/pva/KotlinCodewars/kyu8/TotalAmountOfPoints.kt
+++ src/main/kotlin/org/pva/KotlinCodewars/kyu8/TotalAmountOfPoints.kt
@@ -1,5 +1,11 @@
 package org.pva.KotlinCodewars.kyu8
 
-fun points(games: List<String>): Int {
-    return 1
-}
+fun points(games: List<String>) =
+        games.sumBy {
+            val (x, y) = it.split(":")
+            when {
+                x > y -> 3
+                x < y -> 0
+                else -> 1
+            }
+        }
