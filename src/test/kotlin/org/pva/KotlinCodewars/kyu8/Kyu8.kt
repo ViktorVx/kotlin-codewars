c0952a2f3fbe9fbb259ada084c508dd9aefeec60
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-07-30 16:33:58 +0300

    Grouth of a population

--- src/test/kotlin/org/pva/KotlinCodewars/kyu8/Kyu8.kt
+++ src/test/kotlin/org/pva/KotlinCodewars/kyu8/Kyu8.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
-internal class TotalAmountOfPointsTest {
+internal class Kyu8 {
 
     @BeforeEach
     fun setUp() {
