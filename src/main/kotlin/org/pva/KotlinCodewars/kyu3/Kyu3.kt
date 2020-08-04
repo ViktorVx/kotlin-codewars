c466818c622f6bfae1df4b070e6b62995668edab
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-08-04 18:32:11 +0300

    In work

--- src/main/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
+++ src/main/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
@@ -1,12 +1,33 @@
 package org.pva.KotlinCodewars.kyu3
 
+import java.util.*
+
 
 fun parseRegexp(s: String):String {
-    if (s.length == 1) return "Normal(\'$s\')"
-    if (!s.contains(Regex("\\.|\\*|\\|"))) {
-        var ins = s.replace(Regex("\\(|\\)"), "")
-        ins = ins.toCharArray().map { "Normal(\'$it\')" }.joinToString(", ")
-        return "Str ([$ins])"
+    val q = Stack<Char>()
+    val w = Stack<Char>()
+    val ltr = listOf('a', 'b', '.')
+    val sgn = listOf('|', '*', '(', ')')
+
+    for (c in s.toCharArray()) {
+        if (c in ltr) q.push(c) else w.push(c)
+    }
+    println(q)
+    println(w)
+//    parseStacks(q, w)
+    //------
+//    if (s.length == 1) return "Normal(\'$s\')"
+//    if (!s.contains(Regex("\\.|\\*|\\|"))) {
+//        var ins = s.replace(Regex("\\(|\\)"), "")
+//        ins = ins.toCharArray().map { "Normal(\'$it\')" }.joinToString(", ")
+//        return "Str ([$ins])"
+//    }
+    return parseStacks(q, w)
+}
+
+fun parseStacks(q: Stack<Char>, w: Stack<Char>):String {
+    if (w.pop() == '*') {
+        return "ZeroOrMore (${parseStacks(q, w)})"
     }
     return ""
 }
--- src/test/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
+++ src/test/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
@@ -7,8 +7,10 @@ internal class Kyu3 {
 
     @Test
     fun testing() {
-        assertEquals("Normal(\'a\')", parseRegexp("a"))
-        assertEquals("Str ([Normal(\'a\'), Normal(\'b\')])", parseRegexp("ab"))
-        assertEquals("Str ([Normal(\'a\'), Normal(\'b\')])", parseRegexp("(ab)"))
+//        assertEquals("Normal(\'a\')", parseRegexp("a"))
+//        assertEquals("Str ([Normal(\'a\'), Normal(\'b\')])", parseRegexp("ab"))
+//        assertEquals("Str ([Normal(\'a\'), Normal(\'b\')])", parseRegexp("(ab)"))
+        assertEquals("ZeroOrMore (Or (Normal ('a'), Normal ('b')))", parseRegexp("(a|b)*"))
+//        assertEquals("Or( Str([ Normal('a'), ZeroOrMore( Any() ) ]), Str([ Normal('b'), Normal('b') ]) )", parseRegexp("(a.*)|(bb)"))
     }
 }
