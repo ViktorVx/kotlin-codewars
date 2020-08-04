95f731608693a8e18f3cc89290cf942e0745ca65
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-08-04 13:30:01 +0300

    In work

--- src/main/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
+++ src/main/kotlin/org/pva/KotlinCodewars/kyu3/Kyu3.kt
@@ -1,32 +1,13 @@
 package org.pva.KotlinCodewars.kyu3
 
-import java.math.BigInteger
 
-
-//fun decomp(m:Int): String {
-//    return primeFactors(factorial(m)).groupBy { it }
-//            .map { (k, v) -> if (v.size == 1) "$k" else "$k^$v.size()" }.toString()
-//}
-//
-//fun factorial(m: Int): BigInteger {
-//    var factorial = BigInteger.ONE
-//    for (i in 2..m) {
-//        factorial = factorial.multiply(BigInteger.valueOf(i.toLong()))
-//    }
-//    return factorial
-//}
-//
-//fun primeFactors(n: BigInteger): MutableList<BigInteger> {
-//    var result = mutableListOf<BigInteger>()
-//    var c = BigInteger.TWO
-//    var r: BigInteger = n
-//    while (r > BigInteger.ONE) {
-//        while (r.mod(c) == BigInteger.ZERO) {
-//            result.add(c)
-//            r = r.div(c)
-//        }
-//        c++
-//    }
-//    return result
-//}
+fun parseRegexp(s: String):String {
+    if (s.length == 1) return "Normal(\'$s\')"
+    if (!s.contains(Regex("\\.|\\*|\\|"))) {
+        var ins = s.replace(Regex("\\(|\\)"), "")
+        ins = ins.toCharArray().map { "Normal(\'$it\')" }.joinToString(", ")
+        return "Str ([$ins])"
+    }
+    return ""
+}
 
