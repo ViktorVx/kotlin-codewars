345cb4383c2c3fb2af5997a8ce273cfd29d56d0d
ViktorVx <victorptrv@yandex.ru>
2020-08-06 22:25:53 +0300

    Refactor to class

--- src/main/kotlin/org/pva/KotlinCodewars/Kyu2/Kyu2.kt
+++ src/main/kotlin/org/pva/KotlinCodewars/Kyu2/Kyu2.kt
@@ -1,49 +1,53 @@
 package org.pva.KotlinCodewars.Kyu2
 
 
-fun parseRegexp(s: String):String {
-    println(s)
-    var ss = s.replace(Regex("\\*"), "*N")
-    val ltrL = listOf('a', 'b', '.')
-    val ltrR = listOf('a', 'b', '.', '(')
-    var ssRes = ""
-    for ((i, s) in ss.toCharArray().iterator().withIndex()) {
-        ssRes += if (i == ss.length - 1) s else if (s in ltrL && ss[i + 1] in ltrR) "$s+" else s
-    }
-    return parse(ssRes)
-}
+class RegExpParser(private val input: String) {
 
-fun parse(s: String):String {
-    if (s.length == 1) return if (s == ".") "Any()" else "Normal ('$s')"
-    val ltr = listOf('a', 'b', '.', 'N')
-    val map = mapOf('*' to 1, '+' to 2, '|' to 3)
-    var ind = 0
-    var w = 0
-    var brk = 0
-    for ((i, c) in s.toCharArray().iterator().withIndex()) {
-        if (c in ltr) continue
-        if (c == '(') {
-            brk++
-            continue
+    fun parse(): Regex {
+        val s = input
+        println(s)
+        var ss = s.replace(Regex("\\*"), "*N")
+        val ltrL = listOf('a', 'b', '.')
+        val ltrR = listOf('a', 'b', '.', '(')
+        var ssRes = ""
+        for ((i, s) in ss.toCharArray().iterator().withIndex()) {
+            ssRes += if (i == ss.length - 1) s else if (s in ltrL && ss[i + 1] in ltrR) "$s+" else s
         }
-        if (c == ')') {
-            brk--
-            continue
-        }
-        if (brk != 0) continue
-        if (w <= map[c] ?: error("No such operation")) {
-            w = map[c] ?: error("No such operation")
-            ind = i
-        }
+        return Regex(parse(ssRes))
     }
-    if (ind == 0) return parse(s.substring(1, s.length - 1))
 
-    val oper = s[ind]
-    val left = s.substring(0, ind)
-    val right = s.substring(ind + 1, s.length)
-    return when (oper) {
-        '*' -> "ZeroOrMore (${parse(left)})"
-        '|' -> "Or (${parse(left)}, ${parse(right)})"
-        else -> "Str ([${parse(left)}, ${parse(right)}])"
+    fun parse(s: String):String {
+        if (s.length == 1) return if (s == ".") "Any()" else "Normal ('$s')"
+        val ltr = listOf('a', 'b', '.', 'N')
+        val map = mapOf('*' to 1, '+' to 2, '|' to 3)
+        var ind = 0
+        var w = 0
+        var brk = 0
+        for ((i, c) in s.toCharArray().iterator().withIndex()) {
+            if (c in ltr) continue
+            if (c == '(') {
+                brk++
+                continue
+            }
+            if (c == ')') {
+                brk--
+                continue
+            }
+            if (brk != 0) continue
+            if (w <= map[c] ?: error("No such operation")) {
+                w = map[c] ?: error("No such operation")
+                ind = i
+            }
+        }
+        if (ind == 0) return parse(s.substring(1, s.length - 1))
+
+        val oper = s[ind]
+        val left = s.substring(0, ind)
+        val right = s.substring(ind + 1, s.length)
+        return when (oper) {
+            '*' -> "ZeroOrMore (${parse(left)})"
+            '|' -> "Or (${parse(left)}, ${parse(right)})"
+            else -> "Str ([${parse(left)}, ${parse(right)}])"
+        }
     }
 }
--- src/test/kotlin/org/pva/KotlinCodewars/kyu2/Kyu2.kt
+++ src/test/kotlin/org/pva/KotlinCodewars/kyu2/Kyu2.kt
@@ -2,21 +2,21 @@ package org.pva.KotlinCodewars.kyu3
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.pva.KotlinCodewars.Kyu2.parseRegexp
+import org.pva.KotlinCodewars.Kyu2.RegExpParser
 
 internal class Kyu2 {
 
     @Test
     fun testing() {
-        assertEquals("Normal (\'a\')", parseRegexp("a"))
-        assertEquals("Str ([Normal (\'a\'), Normal (\'b\')])", parseRegexp("ab"))
-        assertEquals("Str ([Normal (\'a\'), Normal (\'b\')])", parseRegexp("(ab)"))
-        assertEquals("ZeroOrMore (Str ([Normal ('a'), Normal ('b')]))", parseRegexp("(ab)*"))
-        assertEquals("ZeroOrMore (Or (Normal ('a'), Normal ('b')))", parseRegexp("(a|b)*"))
-        assertEquals("Or (Str ([Normal ('a'), ZeroOrMore (Any())]), Str ([Normal ('b'), Normal ('b')]))", parseRegexp("(a.*)|(bb)"))
-        assertEquals("Or (Str ([Normal ('a'), Normal ('b')]), Normal ('a'))", parseRegexp("ab|a"))
-        assertEquals("Str ([Normal ('a'), Or (Normal ('b'), Normal ('a'))])", parseRegexp("a(b|a)"))
-        assertEquals("Or (Normal ('a'), ZeroOrMore (Normal ('b')))", parseRegexp("a|b*"))
-        assertEquals("Str ([Normal ('a'), ZeroOrMore (Any())])", parseRegexp("a.*"))
+        assertEquals("Normal (\'a\')", RegExpParser("a").parse().toString())
+        assertEquals("Str ([Normal (\'a\'), Normal (\'b\')])", RegExpParser("ab").parse().toString())
+        assertEquals("Str ([Normal (\'a\'), Normal (\'b\')])", RegExpParser("(ab)").parse().toString())
+        assertEquals("ZeroOrMore (Str ([Normal ('a'), Normal ('b')]))", RegExpParser("(ab)*").parse().toString())
+        assertEquals("ZeroOrMore (Or (Normal ('a'), Normal ('b')))", RegExpParser("(a|b)*").parse().toString())
+        assertEquals("Or (Str ([Normal ('a'), ZeroOrMore (Any())]), Str ([Normal ('b'), Normal ('b')]))", RegExpParser("(a.*)|(bb)").parse().toString())
+        assertEquals("Or (Str ([Normal ('a'), Normal ('b')]), Normal ('a'))", RegExpParser("ab|a").parse().toString())
+        assertEquals("Str ([Normal ('a'), Or (Normal ('b'), Normal ('a'))])", RegExpParser("a(b|a)").parse().toString())
+        assertEquals("Or (Normal ('a'), ZeroOrMore (Normal ('b')))", RegExpParser("a|b*").parse().toString())
+        assertEquals("Str ([Normal ('a'), ZeroOrMore (Any())])", RegExpParser("a.*").parse().toString())
     }
 }
