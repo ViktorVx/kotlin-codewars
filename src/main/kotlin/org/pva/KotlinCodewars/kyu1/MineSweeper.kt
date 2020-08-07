a5d896a8af77d3ecb36b80874e99e2042bf90e42
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-08-07 12:13:17 +0300

    Add mines sweeper

---
+++ src/main/kotlin/org/pva/KotlinCodewars/kyu1/MineSweeper.kt
@@ -0,0 +1,9 @@
+package org.pva.KotlinCodewars.kyu1
+
+class MineSweeper(val board: String, val nMines: Int) {
+
+    fun solve(): String {
+        // Your code here...
+        return ""
+    }
+}
