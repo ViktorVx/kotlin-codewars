9fed3ed4efef0363a7964970eebe35b1e5d9537f
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-08-18 11:15:47 +0300

    Add combination's algorithm

--- src/main/kotlin/org/pva/codewars/kyu1/Permutation.kt
+++ src/main/kotlin/org/pva/codewars/kyu1/Permutation.kt
@@ -1,45 +1,25 @@
 package org.pva.codewars.kyu1
 
+import java.util.*
+
 class Permutation {
 
     companion object {
 
-        // todo реализовать алгоритм генерации сочетаний без повторений
         @JvmStatic fun show(mines:Int, cells:Int) {
-            getNextNum(mines, cells)
+            val arr = arrayOf("A", "B", "C", "D", "E", "F")
+            combinations(arr, 3, 0, arrayOfNulls<String>(3))
         }
 
-        private fun getNextNum(mines:Int, cells:Int) {
-            val alphabet = (0 until cells).toList().toMutableList()
-            println(alphabet)
-            val array = Array(f(cells)/(f(cells - mines) * f(mines))) { IntArray(mines) {0} }
-
-            for (i in 1 until mines) {
-                array[0][i] = alphabet[i]
+        fun combinations(arr: Array<String>, len: Int, startPosition: Int, result: Array<String?>) {
+            if (len == 0) {
+                System.out.println(Arrays.toString(result))
+                return
             }
-
-
-            for ((i, c) in array.iterator().withIndex()) {
-                if (i == 0) continue
-                for (j in mines - 1 downTo 0) {
-                    if (j == mines - 1) {
-                        if (array[i - 1][j] == alphabet.max()) {
-                            c[j - 1] = array[i - 1][j - 1] + 1
-                            c[j] = c[j - 1] + 1
-                        } else {
-                            c[j] = array[i - 1][j] + 1
-                        }
-                    } else {
-                        if (c[j] < array[i - 1][j]) c[j] = array[i - 1][j]
-                    }
-                }
+            for (i in startPosition..arr.size - len) {
+                result[result.size - len] = arr[i]
+                combinations(arr, len - 1, i + 1, result)
             }
-        }
-
-        private fun f(n: Int): Int {
-            var res = 1
-            for (i in 1..n) res *= i
-            return res
         }
     }
 }
