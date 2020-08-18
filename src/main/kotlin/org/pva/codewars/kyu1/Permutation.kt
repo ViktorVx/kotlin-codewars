0a3499838982009bfcc2f83200d57f6bcbb58b76
Петров Виктор Анатольевич <petrov.v.anatolyev@sberbank.ru>
2020-08-18 18:23:14 +0300

    Complete combinations

--- src/main/kotlin/org/pva/codewars/kyu1/Permutation.kt
+++ src/main/kotlin/org/pva/codewars/kyu1/Permutation.kt
@@ -1,19 +1,17 @@
 package org.pva.codewars.kyu1
 
-import java.util.*
-
 class Permutation {
 
     companion object {
 
         @JvmStatic fun show(mines:Int, cells:Int) {
-            val arr = arrayOf("A", "B", "C", "D", "E", "F")
-            combinations(arr, 3, 0, arrayOfNulls<String>(3))
+            val arr = (0 until cells).toList().toIntArray()
+            combinations(arr, mines, 0, arrayOfNulls<Int>(mines))
         }
 
-        fun combinations(arr: Array<String>, len: Int, startPosition: Int, result: Array<String?>) {
+        private fun combinations(arr: IntArray, len: Int, startPosition: Int, result: Array<Int?>) {
             if (len == 0) {
-                System.out.println(Arrays.toString(result))
+                println(result.contentToString())
                 return
             }
             for (i in startPosition..arr.size - len) {
